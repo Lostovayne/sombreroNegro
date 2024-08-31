2e7bce4ae520607a8a0adad8948d5dcc0bc9fb80
casadjes <esmilcasado901@gmail.com>
2024-08-31 03:09:40 -0400

    navbar en construccion

---
+++ src/constants/index.ts
@@ -0,0 +1,22 @@
+export const Menu = [
+  {
+    name: "trabajos",
+    url: "/trabajos",
+  },
+  {
+    name: "nosotros",
+    url: "/nosotros",
+  },
+  {
+    name: "SombreroNegro",
+    url: "/",
+  },
+  {
+    name: "contactos",
+    url: "/contacto",
+  },
+  {
+    name: "Proyectos",
+    url: "/proyectos",
+  },
+];
