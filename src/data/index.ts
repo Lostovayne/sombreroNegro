9202d61fa5ec23cc651e9176d1ec66cccb1a50e2
casadjes <esmilcasado901@gmail.com>
2024-09-02 21:54:49 -0400

    pagina nosotros casi terminada

---
+++ src/data/index.ts
@@ -0,0 +1,36 @@
+export const designerData = {
+  designTeams: {
+    teamOne: {
+      designers: [
+        {
+          image: "https://via.placeholder.com/320x256",
+          fullName: "Davinci Vitruvius",
+          description: "Lorem ipsum dolor sit amet.",
+        },
+        {
+          image: "https://via.placeholder.com/320x256",
+          fullName: "Leonardo Fibonacci",
+          description: "Lorem ipsum dolor sit amet.",
+        },
+      ],
+      teamDescription:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni sapiente consectetur Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni sapiente consectetur Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    teamTwo: {
+      designers: [
+        {
+          image: "https://via.placeholder.com/320x256",
+          fullName: "Michelangelo Buonarroti",
+          description: "Lorem ipsum dolor sit amet.",
+        },
+        {
+          image: "https://via.placeholder.com/320x256",
+          fullName: "Raphael Sanzio",
+          description: "Lorem ipsum dolor sit amet.",
+        },
+      ],
+      teamDescription:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni sapiente consectetur Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni sapiente consectetur Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+  },
+};
