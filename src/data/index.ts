23e5385472b51f6cd69a82ed5b53a8c74b55d953
casadjes <esmilcasado901@gmail.com>
2024-09-04 19:21:13 -0400

    data de contact creada y funcionando correctamente

--- src/data/index.ts
+++ src/data/index.ts
@@ -35,4 +35,25 @@ export const designerData = {
   },
 };
 
-export const contactPageData = {};
+export const contactPageData = {
+  imageSection: {
+    image1: "https://via.placeholder.com/500x300",
+    image2: "https://via.placeholder.com/500x300",
+    image3: "https://via.placeholder.com/500x300",
+    description1:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni sapiente consectetur",
+    description2:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni sapiente consectetur Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni sapiente consectetur Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni sapiente consectetur",
+  },
+  infoSection: {
+    title1: "Texto titulo",
+    description: "pequena descripcion",
+    paragraph:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quisquam consectetur placeat delectus, illo odio quaerat sequi? Quaerat dolor fugiat cum, officia earum, ex, asperiores neque quas a error sint.Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quisquam consectetur placeat delectus, illo odio quaerat sequi? Quaerat dolor fugiat cum, officia earum, ex, asperiores neque quas a error sint.",
+    title2: "Sombrero Negro",
+    contact1: "+56 9 5555 6666",
+    contact2: "+56 9 5555 7777",
+    email1: "sombreronegro.contacto@gmail.com",
+    email2: "segundocorreo@gmail.com",
+  },
+};
